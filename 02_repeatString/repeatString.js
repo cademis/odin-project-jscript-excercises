a00220c531c96d7227a54b071a9319ef81cb7cd6
chris <caitken86@gmail.com>
2023-05-28 23:05:25 +0100

    completed first 4 excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+// create a function that repeats a string by a provided number
 
+const repeatString = function (string, num) {
+  let word = string;
+  for (let i = 1; i < num; i++) {
+    string += word;
+  }
+  return string;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, num) {
+  return arr.filter((x) => x != num);
 };
 
 // Do not edit below this line
