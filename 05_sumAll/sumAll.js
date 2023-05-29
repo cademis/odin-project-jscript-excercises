f04f24eb39a61bcaf75ee3be1dd9b2642322fe09
chris <caitken86@gmail.com>
2023-05-29 09:54:02 +0100

    use temp to swap 2 values of a variable

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
+//use temp to swap 2 values of a variable
+
 const sumAll = function (lowest, highest) {
   let result = 0;
+    if lowest > highest {
+   let temp = highest
+    highest = lowest;
+   lowest = temp;
+     }
   for (let i = lowest; i <= highest; i++) {
     result += i;
   }
