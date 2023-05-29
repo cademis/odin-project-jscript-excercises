9f05b0c531e7ad847621fb06812caa517b00609f
chris <caitken86@gmail.com>
2023-05-29 11:02:29 +0100

    practice using the +number.toFixed(1) method

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+// practice using the +number.toFixed(1) method
+
+const convertToCelsius = function (fahrenheit) {
+  return +(((fahrenheit - 32) * 5) / 9).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  return +((celsius * 9) / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
