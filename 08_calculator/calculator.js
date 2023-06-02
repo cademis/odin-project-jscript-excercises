717d49052f2eada15fdd894a56a217c5ebc0f801
chris <caitken86@gmail.com>
2023-06-02 11:09:38 +0100

    complete calculator excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,13 +6,30 @@ const subtract = function (a, b) {
   return a - b;
 };
 
-const sum = function () {};
+const sum = function (a) {
+  return a.reduce((obj, item) => {
+    return obj + item;
+  }, 0);
+};
 
-const multiply = function () {};
+const multiply = function (array) {
+  return array.reduce((obj, item) => {
+    return obj * item;
+  });
+};
 
-const power = function () {};
+const power = function (a, b) {
+  return a ** b;
+};
 
-const factorial = function () {};
+const factorial = function (num) {
+  if (num === 0) return 1;
+  let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i;
+  }
+  return result;
+};
 
 // Do not edit below this line
 module.exports = {
