5a3bc8178b9c8d04b793a0403d0e5adfb4ec1cbe
chris <caitken86@gmail.com>
2023-06-02 13:14:14 +0100

    complete the getthetitles excercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (data) {
+  let allBooks = [];
+  for (let book of data) {
+    allBooks.push(book.title);
+  }
+  return allBooks;
 };
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
