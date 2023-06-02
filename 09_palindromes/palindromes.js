7a14092fad59f1069ad6ce56abb668ce3c3b04ee
chris <caitken86@gmail.com>
2023-06-02 11:59:44 +0100

    complete palindromes excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  const stringClean = string.toLowerCase().replace(/[^0-9a-zA-Z]/g, "");
+  const explodeString = [...stringClean];
+  return explodeString.reverse().join("") === stringClean;
 };
 
 // Do not edit below this line
